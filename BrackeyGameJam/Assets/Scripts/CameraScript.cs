764553e87947b06c7a714a76b8ada06f0d788629
Aidan Murphy <44793359+iXiphos@users.noreply.github.com>
2022-08-27 20:56:21 -0400

    Updating losing stats on round start

--- BrackeyGameJam/Assets/Scripts/CameraScript.cs
+++ BrackeyGameJam/Assets/Scripts/CameraScript.cs
@@ -9,6 +9,8 @@ public class CameraScript : MonoBehaviour
     private CardManager CM;
     private SceneManager SM;
 
+    int amount_to_lose_on_round_end;
+
     void Start()
     {
         Canvas = GameObject.Find("MainCanvas");
@@ -31,6 +33,12 @@ public class CameraScript : MonoBehaviour
             //Out of scene
             GameObject.Find("CardManager").GetComponent<CardManager>().generateDeck();
             GameObject.Find("CardManager").GetComponent<CardManager>().DrawCard(6);
+            List<Survivor> survivors = GameObject.Find("GameManager").GetComponent<PartyManager>().party;
+            for (int i = 0; i < survivors.Count; i++)
+            {
+                survivors[i].Sanity -= amount_to_lose_on_round_end;
+                survivors[i].Hunger -= amount_to_lose_on_round_end;
+            }
             CM.CheckForIllness();
         }
         else
